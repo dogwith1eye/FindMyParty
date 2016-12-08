67f5343e61e8985a11bbdfdf742fe9e7863bc0cb
dogwith1eye <dogwith1eye@gmail.com>
2016-12-07 19:31:48 -0600

    Added initial stack panels

---
+++ src/FindMyParty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FindMyParty
+//
+//  Created by Dog With 1 Eye on 12/5/16.
+//  Copyright © 2016 Dog With 1 Eye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
